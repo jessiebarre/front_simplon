5454a5634c5cd01e4fe512649a78bc3709d526c0
Jessie Barré <jessie.barre@renault.com>
2020-03-25 11:06:49 +0100

    provide select options with GET country values

---
+++ module-ajax/exercice-1.js
@@ -0,0 +1,33 @@
+
+
+const dropdown = document.getElementById('country-dropdown');
+dropdown.length = 0;
+
+const defaultOption = document.createElement('option');
+defaultOption.text = "Choose a country";
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = "http://localhost:8080/country";
+
+const request = new XMLHttpRequest();
+request.open('GET', url);
+
+request.onload = function() {
+    if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        let option;
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i].name;
+            console.log()
+            option.value = data[i].id;
+            dropdown.add(option);
+        }
+    } else {
+        console.log("error");
+    }
+}
+
+request.send();
