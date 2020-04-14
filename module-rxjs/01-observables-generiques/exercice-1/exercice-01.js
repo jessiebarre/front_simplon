a1dfaec01dbefd6cd48289563d077313c156396a
Jessie Barré <jessie.barre@renault.com>
2020-04-14 14:58:34 +0200

    part 1 with observable ok

--- module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
+++ module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
@@ -3,31 +3,32 @@ import { Observable, fromEvent } from "rxjs";
 let target = document.body;
 let keyDiv = document.getElementById("code");
 
-let observer = {
-  next: (event) => {
-    console.log(event.keyCode);
-    switch (event.keyCode) {
-      case 38:
-        keyDiv.innerHTML = "&#8593;";
-        break;
-      case 40:
-        keyDiv.innerHTML = "&#8595;";
-        break;
-      case 37:
-        keyDiv.innerHTML = "&#8592;";
-        break;
-      case 39:
-        keyDiv.innerHTML = "&#8594;";
-        break;
-      case 13:
-    }
-  },
-  error: (err) => console.log(err),
-  complete: (event) => {
-    if (event.keyCode === 13) {
-      console.log("stop");
-    }
-  },
-};
+let keyEvent$ = fromEvent(target, "keydown");
 
-fromEvent(target, "keydown").subscribe(observer);
+let keyEventSubscription = keyEvent$.subscribe(
+      event => {
+        switch (event.keyCode) {
+          case 38:
+            keyDiv.innerHTML = "&#8593;";
+            break;
+          case 40:
+            keyDiv.innerHTML = "&#8595;";
+            break;
+          case 37:
+            keyDiv.innerHTML = "&#8592;";
+            break;
+          case 39:
+            keyDiv.innerHTML = "&#8594;";
+            break;
+      }
+    })   ;
+  
+fromEvent(target, "keydown")
+.subscribe(
+  event => {
+    if (event.keyCode === 13) {
+      keyEventSubscription.unsubscribe()
+    }
+  }
+
+)
