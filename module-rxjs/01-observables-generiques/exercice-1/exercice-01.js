70456c0fa1f692427fafd65fff36689bc2266826
Jessie Barré <jessie.barre@renault.com>
2020-05-05 17:01:59 +0200

    add part 2 with subject instead of obs

--- module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
+++ module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
@@ -1,9 +1,12 @@
-import { Observable, fromEvent } from "rxjs";
-
 let keyDiv = document.getElementById("code");
 
+/**************** PART 1 ********************/
+/****************************************** 
+import { Observable } from "rxjs";
+
+
 window.addEventListener("DOMContentLoaded", (event) => {
-  const obs = new Observable((observer) => {
+  const myObservable$ = new Observable((observer) => {
     window.addEventListener("keydown", (event) => {
       observer.next(event);
       if (event.keyCode === 13) {
@@ -12,7 +15,36 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 
-  obs.subscribe({
+  myObservable$.subscribe({
+    next(value) {
+      switch (value.keyCode) {
+        case 38:
+          keyDiv.innerHTML = "&#8593;";
+          break;
+        case 40:
+          keyDiv.innerHTML = "&#8595;";
+          break;
+        case 37:
+          keyDiv.innerHTML = "&#8592;";
+          break;
+        case 39:
+          keyDiv.innerHTML = "&#8594;";
+          break;
+      }
+    },
+    complete() {
+      keyDiv.innerHTML = "done";
+    },
+  });
+});*/
+
+/***************************PART 2 **********************/
+
+import { Subject } from "rxjs";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const mySubject$ = new Subject();
+  mySubject$.subscribe({
     next(value) {
       switch (value.keyCode) {
         case 38:
