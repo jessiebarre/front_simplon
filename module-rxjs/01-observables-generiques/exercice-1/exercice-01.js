7d215b43915f11ed8887824ce9c27b41505ea85e
Jessie Barré <jessie.barre@renault.com>
2020-05-05 16:55:12 +0200

    switch from fromEvent to new Obs

--- module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
+++ module-rxjs/01-observables-generiques/exercice-1/exercice-01.js
@@ -1,34 +1,36 @@
 import { Observable, fromEvent } from "rxjs";
 
-let target = document.body;
 let keyDiv = document.getElementById("code");
 
-let keyEvent$ = fromEvent(target, "keydown");
-
-let keyEventSubscription = keyEvent$.subscribe(
-      event => {
-        switch (event.keyCode) {
-          case 38:
-            keyDiv.innerHTML = "&#8593;";
-            break;
-          case 40:
-            keyDiv.innerHTML = "&#8595;";
-            break;
-          case 37:
-            keyDiv.innerHTML = "&#8592;";
-            break;
-          case 39:
-            keyDiv.innerHTML = "&#8594;";
-            break;
+window.addEventListener("DOMContentLoaded", (event) => {
+  const obs = new Observable((observer) => {
+    window.addEventListener("keydown", (event) => {
+      observer.next(event);
+      if (event.keyCode === 13) {
+        observer.complete;
       }
-    })   ;
-  
-fromEvent(target, "keydown")
-.subscribe(
-  event => {
-    if (event.keyCode === 13) {
-      keyEventSubscription.unsubscribe()
-    }
-  }
+    });
+  });
 
-)
+  obs.subscribe({
+    next(value) {
+      switch (value.keyCode) {
+        case 38:
+          keyDiv.innerHTML = "&#8593;";
+          break;
+        case 40:
+          keyDiv.innerHTML = "&#8595;";
+          break;
+        case 37:
+          keyDiv.innerHTML = "&#8592;";
+          break;
+        case 39:
+          keyDiv.innerHTML = "&#8594;";
+          break;
+      }
+    },
+    complete() {
+      keyDiv.innerHTML = "done";
+    },
+  });
+});
