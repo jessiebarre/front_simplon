3add61eb585406fae413d82f247a7c0453822bf5
Jessie Barré <jessie.barre@renault.com>
2020-05-06 11:42:41 +0200

    add part 2 (behaviour subject)

--- module-rxjs/01-observables-generiques/exercice-2/exercice-02.js
+++ module-rxjs/01-observables-generiques/exercice-2/exercice-02.js
@@ -1,5 +1,7 @@
 let keyDiv = document.getElementById("code");
 
+
+
 /**************** PART 1 ********************/
 /****************************************** 
 import { ReplaySubject } from 'rxjs';
@@ -7,7 +9,7 @@ import { ReplaySubject } from 'rxjs';
 window.addEventListener("DOMContentLoader", () => {
 
   const myReplaySubject$ = new ReplaySubject();
-  replaySubject$.subscribe({
+  myReplaySubject$.subscribe({
     next: (value) => {
       switch (value.keyCode) {
         case 38:
@@ -61,3 +63,64 @@ window.addEventListener("DOMContentLoader", () => {
   })
 
 })*/
+/**************** PART 2 ********************/
+/******************************************/ 
+import { BehaviorSubject } from 'rxjs';
+
+window.addEventListener("DOMContentLoader", () => {
+
+  const myBehaviorSubject$ = new BehaviorSubject();
+  myBehaviorSubject$.subscribe({
+    next: (value) => {
+      switch (value.keyCode) {
+        case 38:
+          keyDiv.innerHTML += "&#8593;";
+          break;
+        case 40:
+          keyDiv.innerHTML += "&#8595;";
+          break;
+        case 37:
+          keyDiv.innerHTML += "&#8592;";
+          break;
+        case 39:
+          keyDiv.innerHTML += "&#8594;";
+          break;
+      }
+    },
+    complete() {
+      keyDiv.innerHTML = "done";
+    }
+  })
+
+  window.addEventListener("keydown", (event) => {
+    myBehaviorSubject$.next(event);
+    if (event.keyCode === 13) {
+      myReplaySubject$.complete();
+    }
+  })
+
+  window.addEventListener("click", () => {
+    myBehaviorSubject$.subscribe({
+      next: (value) => {
+        switch (value.keyCode) {
+          case 38:
+            keyDiv.innerHTML += "&#8593;";
+            break;
+          case 40:
+            keyDiv.innerHTML += "&#8595;";
+            break;
+          case 37:
+            keyDiv.innerHTML += "&#8592;";
+            break;
+          case 39:
+            keyDiv.innerHTML += "&#8594;";
+            break;
+        }
+      },
+      complete() {
+        keyDiv.innerHTML = "done";
+      }
+    })
+  })
+
+})
