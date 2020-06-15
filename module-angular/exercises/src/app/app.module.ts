a3d75569f87345521b02f800069194e217fca135
Jessie Barré <jessie.barre@renault.com>
2020-06-15 15:29:22 +0200

    add ex4 : input with ng model directive

--- module-angular/exercises/src/app/app.module.ts
+++ module-angular/exercises/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ExerciceA0Component } from './1-expressions/exercice-a0/exercice-a0.component';
 import { ExerciceA1Component } from './1-expressions/exercice-a1/exercice-a1.component';
 import { ExerciceA2Component } from './1-expressions/exercice-a2/exercice-a2.component';
 import { ExerciceA3Component } from './1-expressions/exercice-a3/exercice-a3.component';
+import { ExerciceA4Component } from './1-expressions/exercice-a4/exercice-a4.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,11 @@ import { ExerciceA3Component } from './1-expressions/exercice-a3/exercice-a3.com
     ExerciceA0Component,
     ExerciceA1Component,
     ExerciceA2Component,
-    ExerciceA3Component
+    ExerciceA3Component,
+    ExerciceA4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
